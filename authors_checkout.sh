27728bc8aea2a6fedce22f884587e42d909c4800
jaredtritz <jtritz@umich.edu>
2013-08-11 23:34:45 -0400

    update the settings files

--- authors_checkout.sh
+++ authors_checkout.sh
@@ -3,11 +3,11 @@
 #export SCRIPT_DIR=$(pwd)
 
 echo 'begin script' 
-cd ~jtritz/bitbucket/ecoach_webapps/mydata4/mts4
+cd ~jtritz/bitbucket/ecoach_webapps/mydata8/mts8
 svn update
-#svn update -r1416 mts.dictionary
+svn update -r1442 mts.dictionary
 source ~jtritz/virtualenv/v1/bin/activate
-python ~jtritz/bitbucket/ecoach_webapps/manage.py collectstatic --noinput --settings=mydata4.settings
+python ~jtritz/bitbucket/ecoach_webapps/manage.py collectstatic --noinput --settings=mydata8.settings
 echo 'end of script'
 
 
