023756d3fe50816558a178b8a225af6516668923
jaredtritz <jtritz@umich.edu>
2013-08-21 12:20:18 -0400

    update the model for Chemistry

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ from djangotailoring.models import SubjectData
 
 # python ../manage.py makemtsmodel > MODEL.OUT (results go below here)
 
-APCOURSES_CHOICES = (
+INT_APCOURSES_CHOICES = (
     ('1', 'Chemistry'),
     ('2', 'Biology'),
     ('3', 'Physics'),
@@ -16,33 +16,63 @@ APCOURSES_CHOICES = (
     ('5', 'Other AP/IB Course'),
 )
 
-_DECLARED_CHOICES = (
-    ('Yes', 'Yes'),
-    ('No', 'No'),
+INT_ATTENDANCE_CHOICES = (
+    ('0', 'Never'),
+    ('1', '1 to 5 times'),
+    ('2', '5 to 10 times'),
+    ('3', '10 or more times'),
+    ('4', "I don't plan on attending lecture"),
 )
 
-_SEMESTERS_COMPLETED_CHOICES = (
-    ('9', 'More than 8 semesters'),
+INT_SLC_CHOICES = (
+    ('1', 'Yes'),
+    ('0', 'No'),
+    ('2', "What's an SLC study group?"),
 )
 
-_CHEM130DISCUSSIONATTENDANCE_CHOICES = (
+INT_DISCUSSIONATTENDANCE_CHOICES = (
     ('0', 'Never'),
     ('1', '1 to 2 times'),
     ('2', '3 to 5 times'),
     ('3', 'More than 5 times'),
 )
 
-_GENDER_CHOICES = (
+CTEXT_COLLEGE_CHOICES = (
+    ('LSA', 'LSA'),
+    ('Engineering', 'Engineering'),
+    ('Kinesiology', 'Kinesiology'),
+    ('Other', 'Other'),
+)
+
+INT_YESNO_CHOICES = (
+    ('1', 'Yes'),
+    ('0', 'No'),
+)
+
+INT_CONFIDENCERANGE_CHOICES = (
+    ('0', 'Not at all confident<br>0'),
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6'),
+    ('7', '7'),
+    ('8', '8'),
+    ('9', '9'),
+    ('10', 'Extremely Confident<br>10'),
+)
+
+CTEXT_GENDER_CHOICES = (
     ('M', 'Male'),
     ('F', 'Female'),
 )
 
-_COMPLETEDINITIALSURVEY_CHOICES = (
-    ('1', 'Survey Completed!'),
-    ('0', 'Survey not completed!'),
+CINT_SEMSESTERS_COMPLETED_CHOICES = (
+    ('9', 'More than 8 semesters'),
 )
 
-_CONCENTRATE_CHOICES = (
+CTEXT_COLLEGE_CONCENTRATE_CHOICES = (
     ('Engineering', 'Engineering'),
     ('Physics', 'Physics/Astrophysics'),
     ('Chemistry', 'Chemistry'),
@@ -60,20 +90,7 @@ _CONCENTRATE_CHOICES = (
     ('Other', 'Other'),
 )
 
-_CHEM130FRIENDS_CHOICES = (
-    ('1', 'Yes, I know others taking the course'),
-    ('0', "No, I don't know others yet"),
-)
-
-_CHEM13REASON_CHOICES = (
-    ('Possible_Concentrate_req', 'I am considering this subject as my concentration'),
-    ('Concentration_req', 'This is a course required by my concentration'),
-    ('Grad_req', 'I need this class to prepare for my graduate/professional program'),
-    ('Credit', 'For a specific credit (NS, QR, etc.)'),
-    ('Interest', "I'm taking this class because of my interest in the subject"),
-)
-
-_POST_COLLEGE_CHOICES = (
+CTEXT_POST_COLLEGE_CHOICES = (
     ('Employment', 'Employment'),
     ('Med_School', 'Medical School or other Health-related Professional School'),
     ('Dent_School', 'Dental School'),
@@ -84,8 +101,13 @@ _POST_COLLEGE_CHOICES = (
     ('Other', 'Other'),
 )
 
-_SUBJECT_INTEREST_CHOICES = (
-    ('0', '0<br>Not at all interested'),
+INT_AGREEDISAGREE_CHOICES = (
+    ('1', 'Agree'),
+    ('0', 'Disagree'),
+)
+
+INT_INTERESTRANGE_CHOICES = (
+    ('0', 'Not at all interested<br>0'),
     ('1', '1'),
     ('2', '2'),
     ('3', '3'),
@@ -95,153 +117,45 @@ _SUBJECT_INTEREST_CHOICES = (
     ('7', '7'),
     ('8', '8'),
     ('9', '9'),
-    ('10', '10<br>Extremely interested'),
+    ('10', 'Extremely interested<br>10'),
 )
 
-_HIGH_SCHOOL_CUMGPA_CHOICES = (
-    ('2_0', 'Less than 2.0'),
-    ('2_1', '2.1'),
-    ('2_2', '2.2'),
-    ('2_3', '2.3'),
-    ('2_4', '2.4'),
-    ('2_5', '2.5'),
-    ('2_6', '2.6'),
-    ('2_7', '2.7'),
-    ('2_8', '2.8'),
-    ('2_9', '2.9'),
-    ('3_0', '3.0'),
-    ('3_1', '3.1'),
-    ('3_2', '3.2'),
-    ('3_3', '3.3'),
-    ('3_4', '3.4'),
-    ('3_5', '3.5'),
-    ('3_6', '3.6'),
-    ('3_7', '3.7'),
-    ('3_8', '3.8'),
-    ('3_9', '3.9'),
-    ('4_0', '4.0'),
+CINT_GPA_CHOICES = (
+    ('20', '2.0 or lower'),
+    ('21', '2.1'),
+    ('22', '2.2'),
+    ('23', '2.3'),
+    ('24', '2.4'),
+    ('25', '2.5'),
+    ('26', '2.6'),
+    ('27', '2.7'),
+    ('28', '2.8'),
+    ('29', '2.9'),
+    ('30', '3.0'),
+    ('31', '3.1'),
+    ('32', '3.2'),
+    ('33', '3.3'),
+    ('34', '3.4'),
+    ('35', '3.5'),
+    ('36', '3.6'),
+    ('37', '3.7'),
+    ('38', '3.8'),
+    ('39', '3.9'),
+    ('40', '4.0'),
 )
 
-SLC_CHOICES = (
-    ('1', 'Yes'),
-    ('0', 'No'),
-    ('2', "What's an SLC study group?"),
-)
-
-_PARENT_ED_CHOICES = (
-    ('Less_HS', 'Less than High School'),
-    ('HS', 'High School/GED'),
-    ('Some_College', 'Some College'),
-    ('2_Year_College', '2-Year College Degree (Associates)'),
-    ('4_Year_College', '4-Year College Degree (BA, BS)'),
-    ('Masters', "Master's Degree"),
-    ('Doctoral', 'Doctoral Degree'),
-    ('Professional', 'Professional Degree (MD, JD)'),
-)
-
-_CUM_GPA_SURVEY_CHOICES = (
-    ('2_0', '2.0 or lower'),
-    ('2_1', '2.1'),
-    ('2_2', '2.2'),
-    ('2_3', '2.3'),
-    ('2_4', '2.4'),
-    ('2_5', '2.5'),
-    ('2_6', '2.6'),
-    ('2_7', '2.7'),
-    ('2_8', '2.8'),
-    ('2_9', '2.9'),
-    ('3_0', '3.0'),
-    ('3_1', '3.1'),
-    ('3_2', '3.2'),
-    ('3_3', '3.3'),
-    ('3_4', '3.4'),
-    ('3_5', '3.5'),
-    ('3_6', '3.6'),
-    ('3_7', '3.7'),
-    ('3_8', '3.8'),
-    ('3_9', '3.9'),
-    ('4_0', '4.0'),
-)
-
-YESNO_CHOICES = (
-    ('1', 'Yes'),
-    ('0', 'No'),
-)
-
-_CHEM13INTEREST_CHOICES = (
-    ('0', '0<br>Not at all interested'),
-    ('1', '1'),
-    ('2', '2'),
-    ('3', '3'),
-    ('4', '4'),
-    ('5', '5'),
-    ('6', '6'),
-    ('7', '7'),
-    ('8', '8'),
-    ('9', '9'),
-    ('10', '10<br>Extremely interested'),
-)
-
-_EMPLOYMENT_CHOICES = (
+CTEXT_EMPLOYMENT_STATUS_CHOICES = (
     ('No_Job', 'I do not have a job'),
     ('Part_Time', 'I work a part-time job (20 hours or less a week)'),
     ('Full_Time', 'I work a full-time job (more than 20 hours a week)'),
 )
 
-_CHEM130ATTENDANCE_CHOICES = (
-    ('0', 'Never'),
-    ('1', '1 to 5 times'),
-    ('2', '5 to 10 times'),
-    ('3', '10 or more times'),
-    ('4', "I don't plan on attending lecture"),
+CTEXT_YES_NO_CHOICES = (
+    ('Yes', 'Yes'),
+    ('No', 'No'),
 )
 
-_CHEM130OWLHOURS_CHOICES = (
-    ('0', "I don't plan on completing the online homework"),
-)
-
-_CONFIDENCE_CHOICES = (
-    ('0', '0<br>Not confident at all'),
-    ('1', '1'),
-    ('2', '2'),
-    ('3', '3'),
-    ('4', '4'),
-    ('5', '5'),
-    ('6', '6'),
-    ('7', '7'),
-    ('8', '8'),
-    ('9', '9'),
-    ('10', '10<br>Extremely confident'),
-)
-
-_REASON_CHOICES = (
-    ('Possible_Concentrate_req', 'I am considering this subject as my concentration'),
-    ('Concentration_req', 'This is a course required by my concentration'),
-    ('Grad_req', 'I need this class to prepare for my graduate/professional program'),
-    ('Credit', 'For a specific credit (NS, QR, etc.)'),
-    ('Interest', "I'm taking this class because of my interest in the subject"),
-)
-
-_CHEM130HOURSSTUDY_CHOICES = (
-    ('0', "I don't plan on studying outside of class."),
-    ('21', 'More than 20'),
-)
-
-CONFIDENCERANGE_CHOICES = (
-    ('0', '0<br>Not confident at all'),
-    ('1', '1'),
-    ('2', '2'),
-    ('3', '3'),
-    ('4', '4'),
-    ('5', '5'),
-    ('6', '6'),
-    ('7', '7'),
-    ('8', '8'),
-    ('9', '9'),
-    ('10', '10<br>Extremely confident'),
-)
-
-_INVOLVED_IN_CHOICES = (
+CTEXT_INVOLVED_IN_CHOICES = (
     ('Greek', 'Greek Life (Sororities/Fraternities)'),
     ('Sports', 'Sports/Club Sports'),
     ('Religious', 'Religious Organizations'),
@@ -251,7 +165,39 @@ _INVOLVED_IN_CHOICES = (
     ('Other', 'Other Student Clubs/Organzations'),
 )
 
-GRADE_DISTRIBUTION_CHOICES = (
+TEXT_CHEMREASON_CHOICES = (
+    ('Possible_Concentrate_req', 'I am considering chemisrty as my concentration'),
+    ('Concentration_req', 'Chemistry is required by my concentration'),
+    ('Grad_req', 'I need this class to prepare for my graduate/professional program'),
+    ('Credit', 'For a specific credit (NS, QR, etc.)'),
+    ('Interest', "I'm taking this class because of my interest in chemistry"),
+)
+
+INT_HOURSSTUDY_CHOICES = (
+    ('21', 'More than 20'),
+)
+
+CTEXT_CLASS_STANDING_CHOICES = (
+    ('Freshman', 'Freshman'),
+    ('Sophomore', 'Sophomore'),
+    ('Junior', 'Junior'),
+    ('Senior', 'Senior'),
+)
+
+INT_YCLASS_CHOICES = (
+    ('1', 'Strongly Disagree<br>1'),
+    ('2', '<br>2'),
+    ('3', '<br>3'),
+    ('4', '<br>4'),
+    ('5', 'Strongly Agree<br>5'),
+)
+
+INT_TODO_CHOICES = (
+    ('0', 'Not Done'),
+    ('1', 'Done'),
+)
+
+INT_GRADE_DISTRIBUTION_CHOICES = (
     ('8', 'A'),
     ('7', 'A-'),
     ('6', 'B+'),
@@ -263,27 +209,7 @@ GRADE_DISTRIBUTION_CHOICES = (
     ('0', 'D+ or lower'),
 )
 
-_APCOURSES_CHOICES = (
-    ('1', 'Yes'),
-    ('0', 'No'),
-)
-
-YCLASS_CHOICES = (
-    ('1', 'Strongly Agree'),
-    ('2', 'Agree'),
-    ('3', 'Neutral'),
-    ('4', 'Disagree'),
-    ('5', 'Strongly Disagree'),
-)
-
-_CLASS_STANDING_CHOICES = (
-    ('Freshman', 'Freshman'),
-    ('Sophomore', 'Sophomore'),
-    ('Junior', 'Junior'),
-    ('Senior', 'Senior'),
-)
-
-_BIRTHMO_CHOICES = (
+CINT_BDAY_MONTH_CHOICES = (
     ('-1', 'Month'),
     ('1', 'January'),
     ('2', 'February'),
@@ -299,11 +225,15 @@ _BIRTHMO_CHOICES = (
     ('12', 'December'),
 )
 
-_COLLEGE_CHOICES = (
-    ('LSA', 'LSA'),
-    ('Engineering', 'Engineering'),
-    ('Kinesiology', 'Kinesiology'),
-    ('Other', 'Other'),
+CTEXT_PARENT_ED_CHOICES = (
+    ('Less_HS', 'Less than High School'),
+    ('HS', 'High School/GED'),
+    ('Some_College', 'Some College'),
+    ('2_Year_College', '2-Year College Degree (Associates)'),
+    ('4_Year_College', '4-Year College Degree (BA, BS)'),
+    ('Masters', "Master's Degree"),
+    ('Doctoral', 'Doctoral Degree'),
+    ('Professional', 'Professional Degree (MD, JD)'),
 )
 
 
@@ -311,12 +241,13 @@ class Source1(SubjectData):
     # add meta property
     class Meta: 
         db_table = 'mydata_source1'
+    ChemPrevious = models.IntegerField(null=True, blank=True)
+    Math_Confidence = models.IntegerField(null=True, blank=True)
     YClass09 = models.IntegerField(null=True, blank=True)
     CHEM130HoursStudy = models.IntegerField(null=True, blank=True)
     CHEM130OWLHours = models.IntegerField(null=True, blank=True)
     Mat_Textbook = models.IntegerField(null=True, blank=True)
     Mat_iClicker = models.IntegerField(null=True, blank=True)
-    CHEM130_Opt_Out = models.IntegerField(null=True, blank=True)
     Mat_Calculator = models.IntegerField(null=True, blank=True)
     Exam_1_Score = models.IntegerField(null=True, blank=True)
     Exam_2_Score = models.IntegerField(null=True, blank=True)
@@ -377,7 +308,7 @@ class Source1(SubjectData):
     Participation_L30 = models.FloatField(null=True, blank=True)
     Participation_L31 = models.FloatField(null=True, blank=True)
     Participation_L32 = models.FloatField(null=True, blank=True)
-    Participation_L33 = models.CharField(max_length=20, null=True, blank=True)
+    Participation_L33 = models.IntegerField(null=True, blank=True)
     Participation_L34 = models.FloatField(null=True, blank=True)
     Participation_L35 = models.FloatField(null=True, blank=True)
     Participation_L36 = models.FloatField(null=True, blank=True)
@@ -395,15 +326,18 @@ class Source1(SubjectData):
     APCoursesTaken__5 = models.NullBooleanField()
     CHEM130SLC = models.IntegerField(null=True, blank=True)
     CHEM130Attendance = models.IntegerField(null=True, blank=True)
+    ToDo_01 = models.IntegerField(null=True, blank=True)
+    ToDo_02 = models.IntegerField(null=True, blank=True)
+    ToDo_03 = models.IntegerField(null=True, blank=True)
+    ToDo_04 = models.IntegerField(null=True, blank=True)
+    ToDo_05 = models.IntegerField(null=True, blank=True)
+    ToDo_06 = models.IntegerField(null=True, blank=True)
+    ToDo_07 = models.IntegerField(null=True, blank=True)
+    ToDo_08 = models.IntegerField(null=True, blank=True)
+    ToDo_09 = models.IntegerField(null=True, blank=True)
+    ToDo_10 = models.IntegerField(null=True, blank=True)
     CHEM130DiscussionAttendance = models.IntegerField(null=True, blank=True)
     CHEM130GoalGrade = models.IntegerField(null=True, blank=True)
-    Reason__Possible_Concentrate_req = models.NullBooleanField()
-    Reason__Concentration_req = models.NullBooleanField()
-    Reason__Grad_req = models.NullBooleanField()
-    Reason__Credit = models.NullBooleanField()
-    Reason__Interest = models.NullBooleanField()
-    Confidence = models.CharField(max_length=2, choices=_CONFIDENCE_CHOICES, null=True, blank=True)
-    Subject_Interest = models.CharField(max_length=2, choices=_SUBJECT_INTEREST_CHOICES, null=True, blank=True)
     YClass26 = models.IntegerField(null=True, blank=True)
     YClass27 = models.IntegerField(null=True, blank=True)
     YClass28 = models.IntegerField(null=True, blank=True)
@@ -435,7 +369,6 @@ class Source1(SubjectData):
     YClass18 = models.IntegerField(null=True, blank=True)
     YClass19 = models.IntegerField(null=True, blank=True)
     YClass20 = models.IntegerField(null=True, blank=True)
-    CompletedInitialSurvey = models.IntegerField(null=True, blank=True)
     YClass21 = models.IntegerField(null=True, blank=True)
     YClass22 = models.IntegerField(null=True, blank=True)
     YClass23 = models.IntegerField(null=True, blank=True)
@@ -459,10 +392,13 @@ class Source1(SubjectData):
     CHEM130Reason__Grad_req = models.NullBooleanField()
     CHEM130Reason__Credit = models.NullBooleanField()
     CHEM130Reason__Interest = models.NullBooleanField()
-    CHEM130Confidence = models.CharField(max_length=2, choices=CONFIDENCERANGE_CHOICES, null=True, blank=True)
-    CHEM130GradeConfidence = models.CharField(max_length=2, choices=CONFIDENCERANGE_CHOICES, null=True, blank=True)
+    CHEM130Confidence = models.IntegerField(null=True, blank=True)
+    CHEM130GradeConfidence = models.IntegerField(null=True, blank=True)
     dist_ID = models.CharField(max_length=20, null=True, blank=True)
-    CHEM130Interest = models.CharField(max_length=2, choices=_CHEM13INTEREST_CHOICES, null=True, blank=True)
+    CHEM130Interest = models.IntegerField(null=True, blank=True)
+    Message_2_Q1 = models.IntegerField(null=True, blank=True)
+    Message_2_Q2 = models.IntegerField(null=True, blank=True)
+    Message_2_Q3 = models.IntegerField(null=True, blank=True)
 
 class EmptySource(SubjectData):
     pass
@@ -474,12 +410,12 @@ class Common1(SubjectData):
     First_Name = models.CharField(max_length=20, null=True, blank=True)
     Last_Name = models.CharField(max_length=20, null=True, blank=True)
     uniqname = models.CharField(max_length=20, null=True, blank=True)
-    Gender = models.CharField(max_length=1, choices=_GENDER_CHOICES, null=True, blank=True)
+    Gender = models.CharField(max_length=1, choices=CTEXT_GENDER_CHOICES, null=True, blank=True)
     BirthDay = models.IntegerField(null=True, blank=True)
     BirthMo = models.IntegerField(null=True, blank=True)
     BirthYr = models.IntegerField(null=True, blank=True)
     Semesters_Completed = models.IntegerField(null=True, blank=True)
-    College = models.CharField(max_length=11, choices=_COLLEGE_CHOICES, null=True, blank=True)
+    College = models.CharField(max_length=11, choices=CTEXT_COLLEGE_CHOICES, null=True, blank=True)
     College_Other = models.CharField(max_length=30, null=True, blank=True)
     Concentrate__Engineering = models.NullBooleanField()
     Concentrate__Physics = models.NullBooleanField()
@@ -496,11 +432,11 @@ class Common1(SubjectData):
     Concentrate__Education = models.NullBooleanField()
     Concentrate__IDK = models.NullBooleanField()
     Concentrate__Other = models.NullBooleanField()
-    Concentrate_Other = models.TextField(null=True, blank=True)
-    Declared = models.CharField(max_length=3, choices=_DECLARED_CHOICES, null=True, blank=True)
-    Class_Standing = models.CharField(max_length=9, choices=_CLASS_STANDING_CHOICES, null=True, blank=True)
-    Cum_GPA_Survey = models.CharField(max_length=3, choices=_CUM_GPA_SURVEY_CHOICES, null=True, blank=True)
-    Employment = models.CharField(max_length=9, choices=_EMPLOYMENT_CHOICES, null=True, blank=True)
+    Concentrate_Other = models.CharField(max_length=30, null=True, blank=True)
+    Declared = models.CharField(max_length=3, choices=CTEXT_YES_NO_CHOICES, null=True, blank=True)
+    Class_Standing = models.CharField(max_length=9, choices=CTEXT_CLASS_STANDING_CHOICES, null=True, blank=True)
+    Cum_GPA_Survey = models.IntegerField(null=True, blank=True)
+    Employment = models.CharField(max_length=9, choices=CTEXT_EMPLOYMENT_STATUS_CHOICES, null=True, blank=True)
     Involved_In__Greek = models.NullBooleanField()
     Involved_In__Sports = models.NullBooleanField()
     Involved_In__Religious = models.NullBooleanField()
@@ -508,9 +444,9 @@ class Common1(SubjectData):
     Involved_In__Volunteering = models.NullBooleanField()
     Involved_In__Music_Art = models.NullBooleanField()
     Involved_In__Other = models.NullBooleanField()
-    Other_Commitment = models.TextField(null=True, blank=True)
-    Post_College = models.CharField(max_length=13, choices=_POST_COLLEGE_CHOICES, null=True, blank=True)
-    Parent_Ed = models.CharField(max_length=14, choices=_PARENT_ED_CHOICES, null=True, blank=True)
-    High_School_CumGPA = models.CharField(max_length=3, choices=_HIGH_SCHOOL_CUMGPA_CHOICES, null=True, blank=True)
+    Other_Commitment = models.CharField(max_length=30, null=True, blank=True)
+    Post_College = models.CharField(max_length=13, choices=CTEXT_POST_COLLEGE_CHOICES, null=True, blank=True)
+    Parent_Ed = models.CharField(max_length=14, choices=CTEXT_PARENT_ED_CHOICES, null=True, blank=True)
+    High_School_CumGPA = models.IntegerField(null=True, blank=True)
 
 
